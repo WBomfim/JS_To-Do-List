907631838971d13644900d1feac56750c4600580
WBomfim <willianbomfim@hotmail.com>
2022-02-16 18:56:35 -0300

    Adicionado Requisito 2

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ function addTitle() {
 }
 
 addTitle();
+
+function addInstruction() {
+  const header = document.querySelector('header');
+  const instruction = document.createElement('p');
+  instruction.id = 'funcionamento';
+  instruction.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
+  header.appendChild(instruction);
+}
+
+addInstruction();
